3e1c580d71b3d5f03bd834f52856c681e0c1c790
Sudhir Kenguva <sudhirkenguva@Sudhirs-MacBook-Air.local>
2020-10-21 09:42:19 +0530

    programs

---
+++ 21st_oct_2020/array_minimum.js
@@ -0,0 +1,27 @@
+// let a = [10, -4, 32, -500, 89];
+
+// let min = Infinity;
+
+// for(var i =0; i < a.length; i++) {
+//   if(min > a[i]) {
+//     min = a[i];
+//   }
+//   console.log(min);
+// }
+
+// array minimun, maximum, sum, product, 2nd min, 2nd max
+
+
+
+
+// console.log(min);
+
+function sum(a, b) {
+  // if(Object.is(Number(a), NaN )|| Object.is(Number(b), NaN)) {
+  //   return 'arguments must be valid numbers';
+  // }
+  return Number(a) + Number(b); 
+}
+
+console.log(sum(5, '5'));
+
---
+++ 21st_oct_2020/asi.js
@@ -0,0 +1,10 @@
+function printObj(name, age) {
+  return {
+    Name: name, 
+    Age: age
+  };
+}
+
+
+var a = printObj("John", 25);
+console.log(a);
